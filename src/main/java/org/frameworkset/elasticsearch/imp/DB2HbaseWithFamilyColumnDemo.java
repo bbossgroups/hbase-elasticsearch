c89f10cfb9ba2edceaece025e8d13a74d407c7f5
yin-bp <yin-bp@163.com>
2023-01-16 21:58:39 +0800

    作业任务回调接口重试次数方法提供默认实现

--- src/main/java/org/frameworkset/elasticsearch/imp/DB2HbaseWithFamilyColumnDemo.java
+++ src/main/java/org/frameworkset/elasticsearch/imp/DB2HbaseWithFamilyColumnDemo.java
@@ -196,10 +196,7 @@ public class DB2HbaseWithFamilyColumnDemo {
 				logger.info(taskCommand.getTaskMetrics().toString(),exception);//打印任务执行情况
 			}
 
-			@Override
-			public int getMaxRetry() {
-				return 0;
-			}
+
 		});
 		importBuilder.addCallInterceptor(new CallInterceptor() {
 			@Override
